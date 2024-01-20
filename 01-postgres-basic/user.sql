30e89472a291e09b11028957919b5d21049fe4ed
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-01-19 23:30:12 -0600

    Adding basic example to play with postgreSQL and docker

---
+++ 01-postgres-basic/user.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE accounts (
+    user_id     serial PRIMARY KEY,
+    username    VARCHAR ( 50 ) UNIQUE NOT NULL,
+    password    VARCHAR ( 50 ) NOT NULL,
+    email       VARCHAR ( 50 ) UNIQUE NOT NULL,
+    created_on  TIMESTAMP NOT NULL,
+    last_login  TIMESTAMP
+);
+
+INSERT INTO accounts (username, password, email, created_on)
+VALUES ('user', 'pass', 'test@email.com', CURRENT_TIMESTAMP);
