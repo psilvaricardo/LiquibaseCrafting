cb65014b48d44dc3bf125a3dcaf9017a2b5b89c9
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-01-24 00:40:44 -0600

    Adding minor fixes to fully integrate Liquibase with docker-compose

--- 01-postgres-basic/liquibase/liquibase-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-/usr/local/bin/wait-for-it.sh postgres:5432 -- liquibase update --changeLogFile=/liquibase/changelog/changelog.xml --url=jdbc:postgresql://postgres:5432/testing --username=testuser --password=docker
